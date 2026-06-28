233ff7fae6b31721e38c94ba0f4ad418a811dc26
Prudnikov Nikolay <n.n.prudnikov@gmail.com>
2026-06-28 04:52:57 +0300

    web: one console.error per error via WebLogger

--- platform/web/os_web.cpp
+++ platform/web/os_web.cpp
@@ -39,6 +39,7 @@
 #include "core/config/project_settings.h"
 #include "core/debugger/engine_debugger.h"
 #include "core/io/file_access.h"
+#include "core/object/script_backtrace.h"
 #include "core/os/main_loop.h"
 #include "core/profiling/profiling.h"
 #include "drivers/unix/dir_access_unix.h"
@@ -49,6 +50,7 @@
 
 #include <dlfcn.h>
 #include <emscripten.h>
+#include <emscripten/console.h>
 #include <cstdlib>
 
 void OS_Web::alert(const String &p_alert, const String &p_title) {
@@ -297,6 +299,63 @@ OS_Web *OS_Web::get_singleton() {
 void OS_Web::initialize_joypads() {
 }
 
+class WebLogger : public Logger {
+public:
+	virtual void logv(const char *p_format, va_list p_list, bool p_err) override _PRINTF_FORMAT_ATTRIBUTE_2_0;
+	virtual void log_error(const char *p_function, const char *p_file, int p_line, const char *p_code, const char *p_rationale, bool p_editor_notify = false, ErrorType p_type = ERR_ERROR, const Vector<Ref<ScriptBacktrace>> &p_script_backtraces = {}) override;
+	virtual ~WebLogger() {}
+};
+
+void WebLogger::logv(const char *p_format, va_list p_list, bool p_err) {
+	if (!should_log(p_err)) {
+		return;
+	}
+
+	const int static_buf_size = 1024;
+	char static_buf[static_buf_size];
+	char *buf = static_buf;
+	va_list list_copy;
+	va_copy(list_copy, p_list);
+	int len = vsnprintf(buf, static_buf_size, p_format, p_list);
+	if (len >= static_buf_size) {
+		buf = (char *)Memory::alloc_static(len + 1);
+		vsnprintf(buf, len + 1, p_format, list_copy);
+	}
+	va_end(list_copy);
+
+	if (len > 0 && buf[len - 1] == '\n') {
+		buf[len - 1] = '\0';
+	}
+
+	if (p_err) {
+		emscripten_err(buf);
+	} else {
+		emscripten_out(buf);
+	}
+
+	if (len >= static_buf_size) {
+		Memory::free_static(buf);
+	}
+}
+
+void WebLogger::log_error(const char *p_function, const char *p_file, int p_line, const char *p_code, const char *p_rationale, bool p_editor_notify, ErrorType p_type, const Vector<Ref<ScriptBacktrace>> &p_script_backtraces) {
+	if (!should_log(true)) {
+		return;
+	}
+
+	const char *err_type = error_type_string(p_type);
+	const char *err_details = (p_rationale && *p_rationale) ? p_rationale : p_code;
+
+	String msg = String(err_type) + ": " + err_details + "\n   at: " + p_function + " (" + p_file + ":" + itos(p_line) + ")";
+	for (const Ref<ScriptBacktrace> &backtrace : p_script_backtraces) {
+		if (!backtrace->is_empty()) {
+			msg += "\n" + backtrace->format(3);
+		}
+	}
+
+	emscripten_err(msg.utf8().get_data());
+}
+
 OS_Web::OS_Web() {
 	char locale_ptr[16];
 	godot_js_config_locale_get(locale_ptr, 16);
@@ -315,7 +374,7 @@ OS_Web::OS_Web() {
 	idb_available = godot_js_os_fs_is_persistent();
 
 	Vector<Logger *> loggers;
-	loggers.push_back(memnew(StdLogger));
+	loggers.push_back(memnew(WebLogger));
 	_set_logger(memnew(CompositeLogger(loggers)));
 
 	FileAccessUnix::close_notification_func = file_access_close_callback;
